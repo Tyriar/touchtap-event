dc0693f518e9ac42e2482df05ac52acc55b0363a
Tyriar <tyriar@tyriar.com>
2014-07-13 08:35:26 -0700

    Add grunt file and build to dist

---
+++ GruntFile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+  grunt.config.init({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.config('clean', {
+    dist: 'dist'
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.config('copy', {
+    dist: {
+      files: {
+        'dist/touchtap-event.js': 'touchtap-event.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.config('uglify', {
+    options: {
+      preserveComments: 'some'
+    },
+    dist: {
+      files: {
+        'dist/touchtap-event.min.js': [
+          'touchtap-event.js'
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('dist', [
+    'clean:dist',
+    'copy:dist',
+    'uglify:dist'
+  ]);
+
+  grunt.registerTask('default', [
+    'dist'
+  ]);
+};
