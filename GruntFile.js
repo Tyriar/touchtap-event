c3a90b4f0c7e720fb543c18bca34c16db8df8d4b
Daniel Imms <tyriar@tyriar.com>
2015-05-21 18:27:37 -0700

    Remove dist/ and related grunt jobs
    Pain to regen, consumer should minify regardless.

--- GruntFile.js
+++ GruntFile.js
@@ -4,55 +4,21 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json')
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.config('clean', {
-    dist: 'dist'
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.config('copy', {
-    dist: {
-      files: {
-        'dist/touchtap-event.js': 'touchtap-event.js'
-      }
-    }
-  });
-
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.config('jasmine', {
     test: {
-      src: 'dist/touchtap-event.js',
+      src: 'touchtap-event.js',
       options: {
         specs: 'test/*-spec.js'
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.config('uglify', {
-    options: {
-      preserveComments: 'some'
-    },
-    dist: {
-      files: {
-        'dist/touchtap-event.min.js': [
-          'touchtap-event.js'
-        ]
-      }
-    }
-  });
-
-  grunt.registerTask('dist', [
-    'clean:dist',
-    'copy:dist',
-    'uglify:dist'
-  ]);
-
   grunt.registerTask('test', [
     'jasmine:test',
   ]);
 
   grunt.registerTask('default', [
-    'dist'
+    'test'
   ]);
 };
